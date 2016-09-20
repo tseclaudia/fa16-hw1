1eb543e521380cf57f5cbaff3dce70dd9e921058
tseclaudia <claudiatse@berkeley.edu>
2016-09-20 03:13:44 -0700

    all functions work

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,11 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    copy = a.collect{|num| num.to_i + 2}
+    copy.select! {|num| num.even?}
+    copy.uniq!
+    copy.select! {|num| num <= 10}
+    copy.reduce(:+)
   end
 end
 
